3a3dc9267317e62d683a62056b5ede6c378b6a98
Letícia Vieira <91754673+1993LETICIA@users.noreply.github.com>
2022-05-17 13:33:42 -0300

    Update num 19.c

--- num 19.c
+++ num 19.c
@@ -1,6 +1,18 @@
-int isEmpty(tfila f){
-	if(f.fim < f.ini)
-	  return 1;
-	else
-	  return 0;  
+include <stdio.h>
+
+#include "bibliotecaPilha.h"
+
+int main(){
+	int dec, base;
+	tpilha p;
+	inicializa(&p);
+	printf("Valor em decimal e a base (2-8):");
+	scanf("%d%d",&dec,&base);
+	while(dec>0){
+		push(&p,dec%base);
+		dec = dec /base; 
+	}
+	printf("Conversao: ");
+	mostraPilha(p);
+	return 0;
 }
