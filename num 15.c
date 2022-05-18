5435cbec23fb3d77847dc887c6b42d72deb3c16e
Letícia Vieira <91754673+1993LETICIA@users.noreply.github.com>
2022-05-17 21:53:29 -0300

    Update num 15.c

--- num 15.c
+++ num 15.c
@@ -1,14 +1,14 @@
-int buscaRg(tfila f, int rgbusca){
+int buscaRg(tfila f, int buscaElem){
 	int pos = 0 , achou =0;
 	
 	while(f.ini<=f.fim){
 		pos++;
-		if(rgbusca==f.fila[f.ini].rg){
+		if(buscaElem==f.fila[f.ini].rg){
 			achou = 1;
 			return pos;
 		}
 		f.ini ++;
-	} // fim hile
+	} 
 	if(achou == 0){
 		return 0;
 	}
